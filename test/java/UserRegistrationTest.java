beb2c5fb664fe8eba9c1af5eea7a6384a58a4c6f
AmarArjunGunjal <amargunjal28@gmail.com>
2019-12-24 12:06:57 +0530

    Refactored:Names of test methods

--- test/java/UserRegistrationTest.java
+++ test/java/UserRegistrationTest.java
@@ -3,21 +3,21 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
     @Test
-    public void whenStartWithCap_shouldReturnMinimum3Character_shouldReturnValid() {
+    public void whenGivenFirstName_whenStartWithCap_shouldReturnMinimum3Character_shouldReturnValid() {
         UserRegistration registration = new UserRegistration();
         String message=registration.firstName("Amar");
         Assert.assertEquals("Valid",message);
     }
 
     @Test
-    public void whenStartWithCap_shouldReturn2character_shouldReturnInvalid() {
+    public void whenGivenFirstName_whenStartWithCap_shouldReturn2character_shouldReturnInvalid() {
         UserRegistration registration = new UserRegistration();
         String message=registration.firstName("Am");
         Assert.assertEquals("Invalid",message);
     }
 
     @Test
-    public void whenStartWithLowerCase_shouldReturnInvalid() {
+    public void whenGivenFirstName_whenStartWithLowerCase_shouldReturnInvalid() {
         UserRegistration registration = new UserRegistration();
         String message=registration.firstName("amar");
         Assert.assertEquals("Invalid",message);
